0eea62f7449bb1a882effe73bd36a6ae34e7b350
min <mmbliv@gmail.com>
2023-04-29 12:44:52 -0500

    comment out content about social authentication

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 const Login = () => {
   const { data: session } = useSession();
+  console.log(session);
   if (session) {
     return (
       <div>
