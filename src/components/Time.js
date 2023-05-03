8a9f004014b9df26929735ab5f2bd601f03a9cef
min <mmbliv@gmail.com>
2023-05-03 14:58:55 -0500

    timer pick ui

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,20 +1,39 @@
 import * as React from "react";
+import { useState } from "react";
 import dayjs from "dayjs";
 // import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import TextField from "@mui/material/TextField";
 
 export default function Timer() {
-  const [value, setValue] = React.useState(dayjs(Date().toLocaleString()));
+  const [value, setValue] = useState(dayjs(Date().toLocaleString()));
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateTimePicker
-        // label="Controlled picker"
-        value={value}
-        onChange={(newValue) => setValue(newValue)}
-      />
+      <div className=" flex flex-col w-full items-center gap-2 h-[calc(100vh_-_157px)] pt-10">
+        <DateTimePicker
+          label="Fell asleep"
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+          className="w-1/3"
+        />
+        <DateTimePicker
+          label="Woke up"
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+          className="w-1/3"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Note"
+          multiline
+          rows={4}
+          defaultValue=""
+          className="w-1/3"
+        />
+      </div>
     </LocalizationProvider>
   );
 }
---
+++ src/hooks/useSleeping.js
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+
+import fetcher from "@/libs/fetcher";
+
+const useSleeping = (userId) => {
+  const url = `/api/sleeping?userId=${userId}`;
+  const { data, error, isLoading, mutate } = useSWR(url, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default usePosts;
