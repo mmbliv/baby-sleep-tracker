73145375c9964be9128290e3b6a68322ba136b82
min <mmbliv@gmail.com>
2023-05-04 11:11:30 -0500

    added checked and close icon

--- src/components/Time.js
+++ src/components/Time.js
@@ -6,24 +6,27 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import TextField from "@mui/material/TextField";
-
+import { AiFillCloseCircle, AiFillCheckCircle } from "react-icons/ai";
 export default function Timer() {
-  const [value, setValue] = useState(dayjs(Date().toLocaleString()));
+  const [fellasleepValue, setFellAsleepValue] = useState(
+    dayjs(Date().toLocaleString())
+  );
+  const [wokeUpValue, setWokeUpValue] = useState(dayjs());
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className=" flex flex-col w-full items-center gap-2 h-[calc(100vh_-_157px)] pt-10">
         <DateTimePicker
           label="Fell asleep"
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
-          className="w-1/3"
+          value={fellasleepValue}
+          onChange={(newValue) => setFellAsleepValue(newValue)}
+          className="w-2/3"
         />
         <DateTimePicker
           label="Woke up"
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
-          className="w-1/3"
+          value={wokeUpValue}
+          onChange={(newValue) => setWokeUpValue(newValue)}
+          className="w-2/3"
         />
         <TextField
           id="outlined-multiline-static"
@@ -31,8 +34,16 @@ export default function Timer() {
           multiline
           rows={4}
           defaultValue=""
-          className="w-1/3"
+          className="w-2/3"
         />
+        <div className=" flex justify-between w-2/3 pt-10">
+          <button className="border-none bg-transparent text-6xl ">
+            <AiFillCloseCircle className=" text-slate-500" />
+          </button>
+          <button className="border-none bg-transparent text-6xl">
+            <AiFillCheckCircle className=" text-slate-500" />
+          </button>
+        </div>
       </div>
     </LocalizationProvider>
   );
