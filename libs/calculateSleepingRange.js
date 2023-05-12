7df1514404a916e714428fce58df8a46cc46a185
min <mmbliv@gmail.com>
2023-05-12 14:10:07 -0500

    fixed bug in calculateSleepingRange

--- libs/calculateSleepingRange.js
+++ libs/calculateSleepingRange.js
@@ -22,7 +22,7 @@ export const calculateSleepingRange = (sleepingData, labels) => {
   for (let i = 0; i < 48; i++) {
     let backgroundColor = "";
     let data = undefined;
-    let lable = undefined;
+    let label = undefined;
     let dataset = {};
     if (i <= 23) {
       backgroundColor = "rgba(255, 99, 132, 0.5)";
@@ -32,27 +32,35 @@ export const calculateSleepingRange = (sleepingData, labels) => {
           for (let j of copy) {
             if (
               j[0]?.split(",")[0] === l &&
-              j[1].length &&
-              dayjs(j[1][0].fell_asleep).format("HH") >= 8 &&
-              dayjs(j[1][0].fell_asleep).format("HH") < 20
+              j[1].length
+              //   dayjs(j[1][0].fell_asleep).format("HH") >= 8 &&
+              //   dayjs(j[1][0].fell_asleep).format("HH") < 20
             ) {
-              const hourF = dayjs(j[1][0].fell_asleep).hour();
-              const minF = dayjs(j[1][0].fell_asleep).minute();
-              const hourW = dayjs(j[1][0].woke_up).hour();
-              const minW = dayjs(j[1][0].woke_up).minute();
-              d = [
-                (hourF + minF / 60).toFixed(2),
-                (hourW + minW / 60).toFixed(2),
-              ];
-              j[1].splice(0, 1);
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") >= 8 &&
+                  dayjs(m.fell_asleep).format("HH") < 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  const index = j[1].indexOf(m);
+                  //   console.log(dayjs(m.fell_asleep).format("HH"));
+                  //   console.log(minF);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "day");
+                  return d;
+                }
+              }
             }
           }
-        return d;
       });
-      if (i === 0) {
-        lable = "day";
-      }
-      dataset = { backgroundColor, data, lable };
+      dataset = { backgroundColor, data, label };
       datasets.push(dataset);
     } else {
       backgroundColor = "rgb(75, 192, 192)";
@@ -60,35 +68,46 @@ export const calculateSleepingRange = (sleepingData, labels) => {
         let d = [];
         if (copy)
           for (let j of copy) {
-            if (
-              (j[0]?.split(",")[0] === l &&
-                j[1].length &&
-                dayjs(j[1][0]?.fell_asleep).format("HH") < 8) ||
-              dayjs(j[1][0]?.fell_asleep).format("HH") > 20
-            ) {
-              const hourF = dayjs(j[1][0].fell_asleep).hour();
-              const minF = dayjs(j[1][0].fell_asleep).minute();
-              const hourW = dayjs(j[1][0].woke_up).hour();
-              const minW = dayjs(j[1][0].woke_up).minute();
-              d = [
-                (hourF + minF / 60).toFixed(2),
-                (hourW + minW / 60).toFixed(2),
-              ];
-              console.log(d);
-              j[1].splice(0, 1);
+            if (j[0]?.split(",")[0] === l && j[1].length) {
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") < 8 ||
+                  dayjs(m.fell_asleep).format("HH") >= 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  //   console.log(dayjs(m.fell_asleep).format("HH"), "night");
+                  const index = j[1].indexOf(m);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "night");
+                  return d;
+                }
+              }
+              //   const hourF = dayjs(j[1][0].fell_asleep).hour();
+              //   const minF = dayjs(j[1][0].fell_asleep).minute();
+              //   const hourW = dayjs(j[1][0].woke_up).hour();
+              //   const minW = dayjs(j[1][0].woke_up).minute();
+              //   d = [
+              //     (hourF + minF / 60).toFixed(2),
+              //     (hourW + minW / 60).toFixed(2),
+              //   ];
+              //   console.log(d, "night");
+              //   j[1].splice(0, 1);
             }
           }
-        return d;
+        // return d;
       });
-      if (i === 47) {
-        lable = "night";
-      }
-      dataset = { backgroundColor, data, lable };
+      dataset = { backgroundColor, data, label };
       datasets.push(dataset);
     }
   }
-  datasets[0].lable = "day";
-  datasets[datasets.length - 1].lable = "night";
-  //   console.log(datasets);
+  datasets[0].label = "day";
+  datasets[datasets.length - 1].label = "night";
   return datasets;
 };
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,40 +1,6 @@
-import React from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  Stack,
-} from "recharts";
-
-const data = [
-  { name: "Group A", low: 10, high: 20 },
-  { name: "Group B", low: 20, high: 30 },
-  { name: "Group C", low: 30, high: 40 },
-];
-
-function StackedRangeChart() {
-  return (
-    <AreaChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Stack>
-        <Area type="monotone" dataKey="high" stroke="#8884d8" fill="#8884d8" />
-        <Area type="monotone" dataKey="low" stroke="#82ca9d" fill="#82ca9d" />
-      </Stack>
-    </AreaChart>
-  );
-}
-
-export default StackedRangeChart;
+const arr = [1, 1, 1].map(() => {
+  for (let i = 0; i < 5; i++) {
+    return 7;
+  }
+});
+console.log(arr);
