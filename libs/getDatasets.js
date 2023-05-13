8e24e4507f3995c4175594c75bd0c7cee31af8b6
min <mmbliv@gmail.com>
2023-05-13 10:21:05 -0500

    thirty days pattern chart done

--- libs/getDatasets.js
+++ libs/getDatasets.js
@@ -1,4 +1,5 @@
 import calculateNap from "./calculateNap";
+import dayjs from "dayjs";
 
 export const getSevenDayWeeklyHoursDatasets = (labels, dailyData) => {
   return [
@@ -130,4 +131,179 @@ export const getThirtyDayTimesDataset = (labels, dailyData) => {
       backgroundColor: "rgb(75, 192, 192)",
     },
   ];
+};
+
+export const getSevenDaypatternDatasets = (sleepingData, labels) => {
+  //   const datasetLength = getTheLengthOfDataset(sleepingData);
+  let copy = [];
+  if (sleepingData) {
+    copy = [...sleepingData];
+  }
+  const datasets = [];
+  for (let i = 0; i < 48; i++) {
+    let backgroundColor = "";
+    let data = undefined;
+    let label = undefined;
+    let dataset = {};
+    if (i <= 23) {
+      backgroundColor = "rgba(255, 99, 132, 0.5)";
+      data = labels.map((l) => {
+        let d = [];
+        if (copy.length)
+          for (let j of copy) {
+            if (
+              j[0]?.split(",")[0] === l &&
+              j[1].length
+              //   dayjs(j[1][0].fell_asleep).format("HH") >= 8 &&
+              //   dayjs(j[1][0].fell_asleep).format("HH") < 20
+            ) {
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") >= 8 &&
+                  dayjs(m.fell_asleep).format("HH") < 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  const index = j[1].indexOf(m);
+                  //   console.log(dayjs(m.fell_asleep).format("HH"));
+                  //   console.log(minF);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "day");
+                  return d;
+                }
+              }
+            }
+          }
+      });
+      dataset = { backgroundColor, data, label };
+      datasets.push(dataset);
+    } else {
+      backgroundColor = "rgb(75, 192, 192)";
+      data = labels.map((l) => {
+        let d = [];
+        if (copy)
+          for (let j of copy) {
+            if (j[0]?.split(",")[0] === l && j[1].length) {
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") < 8 ||
+                  dayjs(m.fell_asleep).format("HH") >= 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  //   console.log(dayjs(m.fell_asleep).format("HH"), "night");
+                  const index = j[1].indexOf(m);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "night");
+                  return d;
+                }
+              }
+            }
+          }
+        // return d;
+      });
+      dataset = { backgroundColor, data, label };
+      datasets.push(dataset);
+    }
+  }
+  datasets[0].label = "day";
+  datasets[datasets.length - 1].label = "night";
+  return datasets;
+};
+
+export const getThirtyDaypatternDatasets = (sleepingData, labels) => {
+  //   const datasetLength = getTheLengthOfDataset(sleepingData);
+  let copy = [];
+  if (sleepingData) {
+    copy = [...sleepingData];
+  }
+  const datasets = [];
+  for (let i = 0; i < 48; i++) {
+    let backgroundColor = "";
+    let data = undefined;
+    let label = undefined;
+    let dataset = {};
+    if (i <= 23) {
+      backgroundColor = "rgba(255, 99, 132, 0.5)";
+      data = labels.map((l) => {
+        let d = [];
+        if (copy.length)
+          for (let j of copy) {
+            if (j[0]?.split(",")[1] === l && j[1].length) {
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") >= 8 &&
+                  dayjs(m.fell_asleep).format("HH") < 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  const index = j[1].indexOf(m);
+                  //   console.log(dayjs(m.fell_asleep).format("HH"));
+                  //   console.log(minF);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "day");
+                  return d;
+                }
+              }
+            }
+          }
+      });
+      dataset = { backgroundColor, data, label };
+      datasets.push(dataset);
+    } else {
+      backgroundColor = "rgb(75, 192, 192)";
+      data = labels.map((l) => {
+        let d = [];
+        if (copy)
+          for (let j of copy) {
+            if (j[0]?.split(",")[1] === l && j[1].length) {
+              for (let m of j[1]) {
+                if (
+                  dayjs(m.fell_asleep).format("HH") < 8 ||
+                  dayjs(m.fell_asleep).format("HH") >= 20
+                ) {
+                  const hourF = dayjs(m.fell_asleep).hour();
+                  const minF = dayjs(m.fell_asleep).minute();
+                  const hourW = dayjs(m.woke_up).hour();
+                  const minW = dayjs(m.woke_up).minute();
+                  d = [
+                    (hourF + minF / 60).toFixed(2),
+                    (hourW + minW / 60).toFixed(2),
+                  ];
+                  //   console.log(dayjs(m.fell_asleep).format("HH"), "night");
+                  const index = j[1].indexOf(m);
+                  j[1].splice(index, 1);
+                  //   console.log(d, "night");
+                  return d;
+                }
+              }
+            }
+          }
+        // return d;
+      });
+      dataset = { backgroundColor, data, label };
+      datasets.push(dataset);
+    }
+  }
+  datasets[0].label = "day";
+  datasets[datasets.length - 1].label = "night";
+  return datasets;
 };
--- src/components/Pattern.js
+++ src/components/Pattern.js
@@ -4,15 +4,18 @@ import { Bar } from "react-chartjs-2";
 import useSleeping from "@/hooks/useSleeping";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import dayjs from "dayjs";
-import { generateWeekLabel } from "../../libs/generateWeekLabel";
+import {
+  generateWeekLabel,
+  generateMonthlyLabel,
+} from "../../libs/generateWeekLabel";
 import { getDailyData } from "../../libs/getDailyData";
 import { useState, useEffect } from "react";
-import calculateNap from "../../libs/calculateNap";
+// import calculateNap from "../../libs/calculateNap";
 import "chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm";
 import {
-  getTheLengthOfDataset,
-  calculateSleepingRange,
-} from "../../libs/calculateSleepingRange";
+  getSevenDaypatternDatasets,
+  getThirtyDaypatternDatasets,
+} from "../../libs/getDatasets";
 
 ChartJS.register(...registerables);
 
@@ -55,25 +58,55 @@ const options = {
   },
 };
 
-const Pattern = () => {
+const Pattern = (props) => {
   const { data: currentUser } = useCurrentUser();
 
   const { data: sleeping, mutate } = useSleeping(currentUser && currentUser.id);
 
   const [dailyData, setDailyData] = useState();
 
-  const [dateSetsArr, setDataSets] = useState([]);
+  const [labels, setLabels] = useState();
 
-  const labels = generateWeekLabel(dayjs().format("ddd"));
+  const [datasets, setDatasets] = useState();
+
+  //   const labels = generateWeekLabel(dayjs().format("ddd"));
+  //   useEffect(() => {
+  //     if (sleeping) {
+  //       setDailyData(getDailyData(sleeping));
+  //     }
+  //   }, [sleeping]);
+
   useEffect(() => {
-    if (sleeping) {
-      setDailyData(getDailyData(sleeping));
+    if (sleeping && props.show === "sevenDay") {
+      setDailyData(getDailyData(sleeping, 7));
     }
-  }, [sleeping]);
+    if (sleeping && props.show === "thirtyDay") {
+      setDailyData(getDailyData(sleeping, 30));
+    }
+  }, [sleeping, props]);
+
+  useEffect(() => {
+    if (props.show === "sevenDay") {
+      setLabels(generateWeekLabel(dayjs().format("ddd")));
+    }
+    if (props.show === "thirtyDay") {
+      setLabels(generateMonthlyLabel(new Date()));
+    }
+  }, [props]);
+
+  useEffect(() => {
+    if (props.show === "sevenDay" && labels && dailyData) {
+      setDatasets(getSevenDaypatternDatasets(dailyData, labels));
+    }
+    if (props.show === "thirtyDay" && labels && dailyData) {
+      setDatasets(getThirtyDaypatternDatasets(dailyData, labels));
+    }
+  }, [props, dailyData, labels]);
 
   const data = {
     labels,
-    datasets: calculateSleepingRange(dailyData, labels),
+    // datasets: calculateSleepingRange(dailyData, labels),
+    datasets: datasets || [],
   };
   return (
     <>
